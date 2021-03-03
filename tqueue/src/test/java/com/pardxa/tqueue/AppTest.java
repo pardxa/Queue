6d5d25bc95109c20f406e4b2f17182e899ca5348
Bill <pardxa@gmail.com>
2021-03-03 10:34:07 -0500

    add Test case

--- tqueue/src/test/java/com/pardxa/tqueue/AppTest.java
+++ tqueue/src/test/java/com/pardxa/tqueue/AppTest.java
@@ -137,4 +137,22 @@ class AppTest {
         assertEquals(Integer.valueOf(9), queue.dequeue());
         assertEquals(Integer.valueOf(5), queue.dequeue());
     }
+    @Test
+    void testExample4() throws InterruptedException {
+        Tqueue<Integer> queue = new Tqueue<>(8, 2);
+        queue.enqueue(Integer.valueOf(82), 8);
+        queue.enqueue(Integer.valueOf(81), 8);
+        queue.enqueue(Integer.valueOf(83), 8);
+        queue.enqueue(Integer.valueOf(84), 8);
+        queue.enqueue(Integer.valueOf(85), 8);
+        queue.enqueue(Integer.valueOf(6), 6);
+        queue.enqueue(Integer.valueOf(5), 5);
+        assertEquals(Integer.valueOf(5), queue.dequeue());
+        assertEquals(Integer.valueOf(6), queue.dequeue());
+        assertEquals(Integer.valueOf(82), queue.dequeue());
+        assertEquals(Integer.valueOf(81), queue.dequeue());
+        assertEquals(Integer.valueOf(83), queue.dequeue());
+        assertEquals(Integer.valueOf(84), queue.dequeue());
+        assertEquals(Integer.valueOf(85), queue.dequeue());
+    }
 }
